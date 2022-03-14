9e88b48f56d4d338356caa4d2a215923745b6965
100SM <bsm2716@gmaile.com>
2022-03-14 10:31:07 +0900

    component #1

---
+++ 2.Component/ex01/src/02/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+import FoodList from "./FoodList";
+
+export default class App extends Component {
+  constructor() {
+    super(arguments);
+
+    this.foods = [
+      {
+        no: 1,
+        name: "Bread",
+        quantity: 10,
+      },
+      {
+        no: 2,
+        name: "Egg",
+        quantity: 20,
+      },
+      {
+        no: 3,
+        name: "Milk",
+        quantity: 5,
+      },
+    ];
+  }
+  render() {
+    return (
+      <ul id="App">
+        <FoodListItem foods={this.foods} />
+      </ul>
+    );
+  }
+}
---
+++ 2.Component/ex01/src/02/FoodList.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import FoodListItem from "./FoodListItem";
+
+export default class FoodList extends Component {
+  render() {
+    const components = [];
+    this.props.foods.forEach(function (food) {
+      components.push(
+        <FoodListItem name={food.name} quantity={food.quantity} />
+      );
+    });
+    return <ul>{components}</ul>;
+  }
+}
