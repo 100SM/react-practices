cf05176ff42368bc4de76038cc53a90951e1a168
100SM <bsm2716@gmaile.com>
2022-03-17 09:09:13 +0900

    event #4

--- 2.Component/ex03/src/02/TitleBar02.js
+++ 2.Component/ex03/src/02/TitleBar02.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React from 'react'
 
 const TitleBar02 = () => {
-  const onClickHandler = (e) => {
-    console.log("TitleBar02 Clicked!");
-  };
-  return (
-    <h1 style={{ cursor: "pointer" }} onClick={onClickHandler}>
-      ex03: Functional Event Handler(Functional Component)
-    </h1>
-  );
-};
+    
+    const onClickHandler = e => {
+        console.log('TitleBar02 Clicked!');
+    } 
 
-export default TitleBar02;
+    return (
+        <h1
+            style={{cursor: 'pointer'}}
+            onClick={onClickHandler}>
+            ex03: Functional Event Handler(Functional Component)
+        </h1>
+    )
+}
+
+export default TitleBar02
