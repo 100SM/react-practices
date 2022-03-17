b0d73b7f1a29e2c789e4a8029bb9d29e1f551605
100SM <bsm2716@gmaile.com>
2022-03-17 17:22:19 +0900

    kanbanboard #final

--- kanbanboard/src/App.js
+++ kanbanboard/src/App.js
@@ -1,24 +1,11 @@
-import React from "react";
-import CardList from "./CardList";
-
-import "./assets/css/styles.css";
+import React from 'react'
+import KanbanBoard from './KanbanBoard';
+import './assets/css/App.css';
 
 const App = () => {
-  const data = require("../public/data.json");
-
   return (
-    <div className="KanbanBoard">
-      <CardList cards={__getListFilter(data, "ToDo")} status="To Do" />
-      <CardList cards={__getListFilter(data, "Doing")} status="In Progress" />
-      <CardList cards={__getListFilter(data, "Done")} status="Done" />
-    </div>
-  );
-};
-
-const __getListFilter = (data, key) => {
-  return data.filter((card) => {
-    return card["status"] === key;
-  });
-};
+    <KanbanBoard />
+  )
+}
 
 export default App;
