a56c14afe50f35470b99a43e742a60a28b6b6507
100SM <bsm2716@gmaile.com>
2022-03-17 12:58:48 +0900

    state #2

---
+++ 2.Component/ex04/src/01/Incrementor01.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+
+export default class extends Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      val: this.props.begin,
+    };
+  }
+
+  onClickButton(e) {
+    console.log(this);
+    this.setState({ val: this.state.val + this.props.step });
+  }
+
+  render() {
+    return (
+      <div>
+        <button onClick={this.onClickButton.bind(this)}>
+          <strong>+</strong>
+        </button>{" "}
+        <span>{this.state.val}</span>
+      </div>
+    );
+  }
+}
