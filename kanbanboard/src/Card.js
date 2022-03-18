060d3ae0c6904ed32701859bd43576179f2eb44e
100SM <bsm2716@gmaile.com>
2022-03-18 16:03:22 +0900

    add title onClick event => showDetails

--- kanbanboard/src/Card.js
+++ kanbanboard/src/Card.js
@@ -4,14 +4,23 @@ import TaskList from "./TaskList";
 
 const Card = ({ title, description, tasks }) => {
   const [showDetails, setShowDetails] = useState(false);
-  
+
   return (
     <div className={styles.Card}>
-      <div className={styles.Card__Title}>{title}</div>
-      <div className={styles.Card__Details}>
-        {description}
-        <TaskList tasks={tasks} />
+      <div
+        className={styles.Card__Title}
+        onClick={() => {
+          setShowDetails(!showDetails);
+        }}
+      >
+        {title}
       </div>
+      {showDetails ? (
+        <div className={styles.Card__Details}>
+          {description}
+          <TaskList tasks={tasks} />
+        </div>
+      ) : null}
     </div>
   );
 };
