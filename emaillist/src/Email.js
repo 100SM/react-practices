8d43a58308ff05593bea17a8acfffa8c891f04d5
100SM <bsm2716@gmaile.com>
2022-03-21 14:57:40 +0900

    prop validation #final

--- emaillist/src/Email.js
+++ emaillist/src/Email.js
@@ -1,18 +1,18 @@
-import React from "react";
-import styles from "./assets/scss/Email.scss";
-import Emaillist from "./Emaillist";
+import React from 'react'
+import styles from './assets/scss/Email.scss';
+import PropTypes from 'prop-types';
 
-const Email = ({firstName, lastName, email}) => {
+export default function Email({firstName, lastName, email}) {
   return (
     <li className={styles.Email}>
-      <h4>
-        {firstName}
-        {lastName}
-      </h4>
-      <br />
+      <h4>{`${firstName} ${lastName}`}</h4>
       <span>{email}</span>
     </li>
   );
-};
+}
 
-export default Email;
+Email.propType = {
+  firstName: PropTypes.string.isRequired,
+  lastName: PropTypes.string.isRequired,  
+  email: PropTypes.string.isRequired
+}
--- emaillist/src/Emaillist.js
+++ emaillist/src/Emaillist.js
@@ -1,20 +1,22 @@
-import React from "react";
-import styles from "./assets/scss/Emaillist.scss";
-import Email from "./Email";
+import React from 'react'
+import styles from './assets/scss/Emaillist.scss';
+import Email from './Email';
+import PropTypes from 'prop-types';
 
-const Emaillist = ({ emails }) => {
+export default function Emaillist({emails}) {
   return (
-    <ul className={styles.Emaillist}>
-      {emails.map((email) => (
-        <Email
-          no={email.no}
-          firstName={email.firstName}
-          lastName={email.lastName}
-          email={email.email}
-        />
-      ))}
-    </ul>
-  );
-};
+        <ul className={styles.Emaillist}>
+        {
+            emails.map(email => <Email
+                                    key={email.no}
+                                    firstName={email.firstName}
+                                    lastName={email.lastName}
+                                    email={email.email} />)
+        }
+        </ul>
+    )
+}
 
-export default Emaillist;
+Emaillist.propTypes = {
+    emails: PropTypes.arrayOf(PropTypes.shape(Email.propType))
+}
