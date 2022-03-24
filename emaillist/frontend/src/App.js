4304faaa0b922f4924c5136abb710364d2a60fe6
100SM <bsm2716@gmaile.com>
2022-03-24 12:44:17 +0900

    emaillist build #4

--- emaillist/frontend/src/App.js
+++ emaillist/frontend/src/App.js
@@ -1,38 +1,55 @@
-import React, {useEffect, useState} from 'react'
-import './assets/scss/App.scss';
-import RegisterForm from './RegisterForm';
-import SearchBar from './SearchBar';
-import Emaillist from './Emaillist';
-import data from './assets/json/data.json';
+import React, { useEffect, useState } from "react";
+import "./assets/scss/App.scss";
+import RegisterForm from "./RegisterForm";
+import SearchBar from "./SearchBar";
+import Emaillist from "./Emaillist";
+import data from "./assets/json/data.json";
 
 const App = () => {
   const [emails, setEmails] = useState(data);
 
-
-  useEffect(async ()=>{
-    const response = await fetch("/api",{
+  useEffect(async () => {
+    const response = await fetch("/api", {
       method: "get",
       headers: {
         "Content-Type": "application/json",
-        "Accept": "application/json"
+        Accept: "application/json",
       },
-      body: null
+      body: null,
     });
 
-    console.log(response);
-  },[]);
+    if (response.ok) {
+      console.log("error: ", response.status, response.statusText);
+      return;
+    }
 
-  const notifyKeywordChange = function(keyword) {
-    setEmails(data.filter(e => e.firstName.indexOf(keyword) != -1 || e.lastName.indexOf(keyword) != -1 || e.email.indexOf(keyword) != -1));
-  }
+    const json = await response.json();
+    if (json.result !== "success") {
+      console.log("error: ", json.message);
+      return;
+    }
+
+    setEmails(json.data);
+  }, []);
+
+  const notifyKeywordChange = function (keyword) {
+    setEmails(
+      data.filter(
+        (e) =>
+          e.firstName.indexOf(keyword) != -1 ||
+          e.lastName.indexOf(keyword) != -1 ||
+          e.email.indexOf(keyword) != -1
+      )
+    );
+  };
 
   return (
-    <div className={'App'}>
+    <div className={"App"}>
       <RegisterForm />
-      <SearchBar callback={notifyKeywordChange}/>
+      <SearchBar callback={notifyKeywordChange} />
       <Emaillist emails={emails} />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
