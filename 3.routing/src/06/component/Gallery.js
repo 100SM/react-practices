3d5e503ca67e68e026601ccaca4d575353c515fa
100SM <bsm2716@gmaile.com>
2022-03-31 12:20:11 +0900

    routing #3

--- 3.routing/src/06/component/Gallery.js
+++ 3.routing/src/06/component/Gallery.js
@@ -1,12 +1,13 @@
-import React from "react";
-import styles from "../assets/scss/component/Gallery.scss";
+import React, {Fragment} from 'react';
+import SiteLayout from '../layout/SiteLayout';
+import styles from '../assets/scss/component/Gallery.scss';
 
 export default function Gallery() {
-  return (
-    <SiteLayout>
-      <div className={styles.Gallery}>
-        <h2>Gallery</h2>
-      </div>
-    </SiteLayout>
-  );
+    return (
+        <SiteLayout>
+            <div className={styles.Gallery}>
+                <h2>Gallery</h2>
+            </div>
+        </SiteLayout>
+    );
 }
--- 3.routing/src/06/layout/SiteLayout.js
+++ 3.routing/src/06/layout/SiteLayout.js
@@ -1,18 +1,20 @@
-import React, { Fragment } from "react";
-import styles from "../assets/scss/layout/Content.scss";
-import Footer from "./Footer";
-import Header from "./Header";
-import Navigation from "./Navigation";
+import React, { Fragment } from 'react'
+import Header from './Header';
+import Navigation from './Navigation';
+import Footer from './Footer';
+import styles from '../assets/scss/layout/Content.scss';
 
-SiteLayout = ({ children }) => {
+const SiteLayout = ({children}) => {
   return (
-    <Fragment>
-      <Header />
-      <div className={styles.Content}>{children}</div>
-      <Navigation />
-      <Footer />
+      <Fragment>
+        <Header />
+        <div className={styles.Content}>
+            {children}
+        </div>
+        <Navigation />
+        <Footer />
     </Fragment>
-  );
-};
+  )
+}
 
-export default SiteLayout;
+export default SiteLayout
